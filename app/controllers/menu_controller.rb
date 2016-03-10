e0950806c1f3abe973f7e33a3cf18ed5545f3b11
Harry nguyen <dnncntt@gmail.com>
2016-03-10 17:50:09 +0700

    add section

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -1,4 +1,6 @@
 class MenuController < ApplicationController
   def index
+  	@sections = %w(Breakfast Lunch Dinner Supper)
+  	@food_items = FoodItem.by_section(params[:section])
   end
 end
--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -1,2 +1,13 @@
 class FoodItem < ActiveRecord::Base
+	def image_url_or_default
+		if image_url.present?
+			image_url
+		else
+			"http://loremflickr.com/320/240/#{CGI.escape name}"
+		end
+	end
+
+	def self.by_section(section)
+		where(section: section)
+	end
 end
