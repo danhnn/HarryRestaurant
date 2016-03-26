65507fb7be6b787784078660099d8035727ce16a
Harry nguyen <dnncntt@gmail.com>
2016-03-26 11:22:04 +0700

    remove perosnal information

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,8 +46,8 @@ Rails.application.configure do
   address:              'smtp.gmail.com',
   port:                 587,
   domain:               'gmail.com',
-  user_name:            'bestseoisme@gmail.com',
-  password:             'ak1988it',
+  user_name:            'secret@gmail.com',
+  password:             'secret',
   authentication:       'plain',
   enable_starttls_auto: true  }
 end
